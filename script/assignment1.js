dc08d55ee83fcff1e7d437637c4b9d97a5339ed1
ryunghi <scor7910@gmail.com>
2018-02-04 17:55:39 -0500

    add bmr function and result div

--- script/assignment1.js
+++ script/assignment1.js
@@ -16,30 +16,42 @@ function openTab(evt, cityName) {
 }
 
 /*function to calculate BMR in Metric*/
-function calculateBMOInMetric() {
+function calculateBMRInMetric() {
   var gender = document.getElementById("gender").value;
-  var age = document.getElementById("age");
-  var height = document.getElementById("height");
-  var weight = document.getElementById("weight");
+  var age = document.getElementById("age").value;
+  var height = document.getElementById("height").value;
+  var weight = document.getElementById("weight").value;
+  var bmrResult = 0;
 
-  if(gender =="Male") {
-
+  if (gender == "male") {
+      bmrResult = 10*weight + 6.25*height - 5*age + 5;
   }
   else {
-
+      bmrResult = (10 * weight) + (6.25 * height) - (5 * age) - 161;
   }
+  displayBMRResult(bmrResult);
+  return false;
 }
 
 /*function to calculate BMR in Imperial*/
-function calculateBMOInImperial {
+function calculateBMRInImperial() {
   var gender = document.getElementById("gender").value;
-  var age = document.getElementById("age");
-  var height = document.getElementById("height");
-  var weight = document.getElementById("weight");
-
-  if(gender =="Male") {
-
+  var age = document.getElementById("age").value;
+  var height = document.getElementById("height").value;
+  var weight = document.getElementById("weight").value;
+  var bmrResult =0;
+  if (gender == "male") {      
+      bmrResult = 66 + ( 6.23*weight ) + ( 12.7*height ) - ( 6.8 * age )
   }
   else {
+      bmrResult = 655 + ( 4.35*weight) + ( 4.7*height) - ( 4.7*age )
   }
+  displayBMRResult(bmrResult);
+  return false;
+}
+
+/*function to display BMR result*/
+function displayBMRResult(result) {
+    document.getElementById("bmrResult").innerHTML = " <h3>Your BMR result is <h2>" + result + "</h2></h3>";
+    document.getElementById('bmrResult').style.display = "block";
 }
