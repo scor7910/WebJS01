c2ddcfaa554a1128575b4168c9c9838edf6736c5
scor7910 <scor7910@gmail.com>
2018-02-04 18:35:37 -0500

    add input valid routine. fix input id bug

--- script/assignment1.js
+++ script/assignment1.js
@@ -17,11 +17,16 @@ function openTab(evt, cityName) {
 
 /*function to calculate BMR in Metric*/
 function calculateBMRInMetric() {
-  var gender = document.getElementById("gender").value;
-  var age = document.getElementById("age").value;
-  var height = document.getElementById("height").value;
-  var weight = document.getElementById("weight").value;
+  var gender = document.getElementById("genderM").value;
+  var age = document.getElementById("ageM").value;
+  var height = document.getElementById("heightM").value;
+  var weight = document.getElementById("weightM").value;
   var bmrResult = 0;
+  if (validateUserInput(age) == false || validateUserInput(height) == false ||
+     validateUserInput(weight) == false) {
+      alert("Input correct value.");
+      return false;
+  }
 
   if (gender == "male") {
       bmrResult = 10*weight + 6.25*height - 5*age + 5;
@@ -29,25 +34,40 @@ function calculateBMRInMetric() {
   else {
       bmrResult = (10 * weight) + (6.25 * height) - (5 * age) - 161;
   }
+  
   displayBMRResult(bmrResult);
   return false;
 }
 
 /*function to calculate BMR in Imperial*/
 function calculateBMRInImperial() {
-  var gender = document.getElementById("gender").value;
-  var age = document.getElementById("age").value;
-  var height = document.getElementById("height").value;
-  var weight = document.getElementById("weight").value;
-  var bmrResult =0;
+  var gender = document.getElementById("genderI").value;
+  var age = document.getElementById("ageI").value;
+  var height = document.getElementById("heightI").value;
+  var weight = document.getElementById("weightI").value;
+  var bmrResult = 0;
+
+  if (validateUserInput(age) == false || validateUserInput(height) == false ||
+    validateUserInput(weight) == false) {
+      alert("Input correct value.");
+      return false;
+  }
   if (gender == "male") {      
-      bmrResult = 66 + ( 6.23*weight ) + ( 12.7*height ) - ( 6.8 * age )
+      bmrResult = 66 + ( 6.23*weight ) + ( 12.7*height ) - ( 6.8 * age );
   }
   else {
-      bmrResult = 655 + ( 4.35*weight) + ( 4.7*height) - ( 4.7*age )
+      bmrResult = 655 + ( 4.35*weight) + ( 4.7*height) - ( 4.7*age );
   }
   displayBMRResult(bmrResult);
   return false;
+}
+
+/*function to validate user input*/
+function validateUserInput(inputValue) {
+    if (isNaN(inputValue) || inputValue < 1)
+        return false;
+    else
+        return true;
 }
 
 /*function to display BMR result*/
